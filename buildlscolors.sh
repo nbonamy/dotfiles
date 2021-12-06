750c7429a441f3be4fae0fa9de5e2f840ffa9ee3
Nicolas Bonamy <nicolas@bonamy.fr>
2021-12-06 15:48:13 -0600

    ls colors templating

--- buildlscolors.sh
+++ buildlscolors.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "export $(grep -v .test .dircolors | dircolors - | head -1)" > .lscolors
+# read our templates
+dircolors=$(<.dircolors)
+
+# now read our colors
+while read -r line; do
+  if [ "$line" != "" ] && [ "${line:0:1}" != "#" ]; then
+    name=$(echo $line | cut -d ' ' -f 1)
+    value=$(echo $line | cut -d ' ' -f 2)
+    dircolors=$(echo "$dircolors" | sed -e "s/{$name}/$value/g" -)
+  fi
+done < colors
+
+dircolors=$(echo "$dircolors" | dircolors - | head -1)
+echo "export $dircolors" > .lscolors
