6bdbb579322d2543db982ffade93220ce5149e02
Nicolas Bonamy <nicolas@Bonamy.fr>
2023-04-07 07:42:58 -0500

    brew installs

---
+++ install-brews.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+brew install binutils
+brew install coreutils
+brew install vim
+brew install git
+brew install node
+brew install python
+
+brew tap homebrew/cask-fonts 
+brew install font-sf-mono
+brew install font-cascadia-code
+brew install font-fira-code
+brew install font-roboto
+brew install font-roboto-mono
